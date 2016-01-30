8a1e62b28dcf13f96cba633727ccf95d3c9561c6
happycaroline <carolinezt@outlook.com>
2016-01-30 23:26:13 +0800

    1

--- js/common.js
+++ js/common.js
@@ -7,4 +7,9 @@ $(function(){
         $('.gender').removeClass('gender-checked')
         $(this).parent().addClass('gender-checked')
     })
+    $('.purple-btn').click(function(e) {
+    	e.preventDefault();//阻止提交
+    	$('.tips').html('提交成功').show();
+    	setTimeout("$('.tips').hide()",1000)
+    })
 });
